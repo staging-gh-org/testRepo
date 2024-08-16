a58c5e5f2e9c6ea512ef140919251d524c5f906d
iubayb <33900218+iubayb@users.noreply.github.com>
2024-08-16 11:11:37 +0400

    add translations

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      {{ $t('hello') }}
       {{ $t('12_months') }}
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+
     </a>
+    {{ $t('hello') }}
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
