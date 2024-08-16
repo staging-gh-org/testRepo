e890bef32e89cb909ce99bdcaf73849f1b488ff8
Ayoub <33900218+iubayb@users.noreply.github.com>
2024-08-16 10:56:55 +0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
       {{ $t('hello') }}
+      {{ $t('12_months') }}
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
